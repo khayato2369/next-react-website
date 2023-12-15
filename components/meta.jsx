e8a08c5ff3f78c54890e65bf5ba9183c3d08ad87
khayato2369 <131949786+khayato2369@users.noreply.github.com>
2023-12-15 16:20:08 +0900

    198

---
+++ components/meta.jsx
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { siteMeta } from 'lib/constants'
+
+const {siteTitle , siteDesc, siteUrl, siteLocale, siteType, siteIcon} = siteMeta
+
+
+export default function Meta({ pageTitle, pageDesc}) {
+
+    const title = pageTitle ? '${pageTitle} | ${siteTitle}' : siteTitle
+    
+    const desc = pageDesc ?? siteDesc
+
+
+    return(
+        <Head>
+            <title>{pageTitle} | {siteTitle}    </title>
+            <meta property='og:title' content={'${pageTitle} | ${siteTitle}'}/>
+            <meta name='description' content={desc} />
+            <meta property='og:description' content={desc} />
+            
+            <link rel='canonical' href={url} />
+            <meta property='og:url' content={url} />
+
+            <meta property='og:site_name'content={siteTitle} />
+            <meta property='og:type' content={siteType} />
+            <meta property='og:locale' content={siteLocale} />
+
+            <link rel='icon' href={siteIcon} />
+            <link rel='apple-touch-icon' href={siteIcon} />
+
+        </Head>
+    )
+}
