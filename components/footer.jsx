e4e9cb2b21bd26d252e32ae737f44f50f96070d0
khayato2369 <131949786+khayato2369@users.noreply.github.com>
2023-12-15 15:24:05 +0900

    a

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,13 +1,14 @@
 import Logo from 'components/logo'
 import styles from '../styles/footer.module.css'
 import Container from './container'
+import Social from 'components/social'
 export default function Footer() {
     return (  
     <footer className={styles.wrapper}> 
     <Container>
     <div className={styles.flexContainer}>
         <Logo />
-        [ソーシャル]
+        <Social />
         </div>
     </Container>
     </footer>
