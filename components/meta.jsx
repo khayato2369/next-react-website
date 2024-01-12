2a16532bdbfa75f9e556d77eadff226b84293fec
khayato2369 <131949786+khayato2369@users.noreply.github.com>
2024-01-12 16:30:26 +0900

    211

--- components/meta.jsx
+++ components/meta.jsx
@@ -1,21 +1,30 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { siteMeta } from 'lib/constants'
+import siteImg from 'images/ogp.jpg'
 
 const {siteTitle , siteDesc, siteUrl, siteLocale, siteType, siteIcon} = siteMeta
 
 
-export default function Meta({ pageTitle, pageDesc}) {
+export default function Meta({ pageTitle, pageDesc,pageImg,pageImgW,pageImgH}) {
 
     const title = pageTitle ? '${pageTitle} | ${siteTitle}' : siteTitle
     
     const desc = pageDesc ?? siteDesc
 
+    const router = useRouter()
+    const url = '${siteUrl}${router.asPath}'
+
+
+    const img = pageImg || siteImg.src
+    const imgW = pageImgW || siteImg.width
+    const imgH = pageImgH || siteImg.height
+    const imgUrl = img.startsWith('https') ? img : `${siteUrl}${img}`
 
     return(
         <Head>
             <title>{pageTitle} | {siteTitle}    </title>
-            <meta property='og:title' content={'${pageTitle} | ${siteTitle}'}/>
+            <meta property='og:title' content={`${pageTitle} | ${siteTitle}`}/>
             <meta name='description' content={desc} />
             <meta property='og:description' content={desc} />
             
@@ -28,6 +37,11 @@ export default function Meta({ pageTitle, pageDesc}) {
 
             <link rel='icon' href={siteIcon} />
             <link rel='apple-touch-icon' href={siteIcon} />
+
+            <meta property='og:image' content={imgUrl} />
+            <meta property='og:image:width' content={imgW} />
+            <meta property='og:image:height' content={imgH} />
+            <meta name='twitter:card' content='summary_large_image' />
 
         </Head>
     )
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,8 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+import { siteMeta } from 'lib/constants'
+const { siteLage } = siteMeta
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang={siteLage}>
       <Head />
       <body>
         <Main />
