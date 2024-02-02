283135bb05dd4713b72dad89df46027412039022
khayato2369 <131949786+khayato2369@users.noreply.github.com>
2024-02-02 16:12:53 +0900

    260

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -1,5 +1,6 @@
-import{ getPostBySlug} from 'lib/api'
+import{ getPostBySlug,  getAllSlugs} from 'lib/api'
 import{ extractText } from 'lib/extract-text'
+import { prevNextPost } from 'lib/prev-next-post'
 import Meta from 'components/meta'
 import Container from 'components/container' 
 import PostHeader from 'components/post-header'
@@ -8,16 +9,29 @@ import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-colum
 import ConvertBody from 'components/convert-body'
 import PostCategories from 'components/post-categories'
 import Image from 'next/image'
+import { getPlaiceholder } from 'plaiceholder'
 
-export default function Schedule( {
+import { eyecatchLocal } from 'lib/constants'
+
+export default function Post( {
     title,
     publish,
     content,
     eyecatch,
     categories,
+    description,
+    prevPost,
+    nextPost,
 }){
     return(
         <Container>
+            <Meta
+                pageTitle={title}
+                pageDesc={description}
+                pageImg={eyecatch.url}
+                pageImgW={eyecatch.width}
+                pageImgH={eyecatch.height}
+            />
             <article>
                 <PostHeader  title={title} subtitle="Blog Article" publish={publish} />
                 <figure>
@@ -29,6 +43,8 @@ export default function Schedule( {
                     height={eyecatch.height}
                     sizes="(min-width: 1152px)1152px,100vw"
                     priority
+                    placeholder='blur'
+                    blurDataURL={eyecatch.blurDataURL}
                     />
                     </figure>
                     
@@ -42,27 +58,46 @@ export default function Schedule( {
                             <PostCategories categories={categories} />
                         </TwoColumnSidebar>
                     </TwoColumn>
-
+                    <div>{prevPost.title} {prevPost.slug}</div>
+                    <div>{nextPost.title} {nextPost.slug}</div>
             </article>
         </Container>
     )
 }
 
-export async function getStaticProps() {
-    const slug = 'schedule'
+export async function getStaticPaths(){
+    const allSlugs = await getAllSlugs()
+    return {
+        paths: allSlugs.map(({ slug }) => `/blog/${slug}`),
+        fallback: false,
+    }
+}
+
+export async function getStaticProps(context) {
+    const slug = context.params.slug
 
     const post = await getPostBySlug(slug)
 
     const description = extractText(post.content)
+
+    const eyecatch = post.eyecatch ?? eyecatchLocal
+
+    const { base64 } = await getPlaiceholder(eyecatch.url)
+    eyecatch.blurDataURL = base64
+
+    const allSlugs = await getAllSlugs()
+    const [prevPost, nextPost] = prevNextPost(allSlugs, slug)
 
     return {
         props:{
             title: post.title,
             publish: post.publishDate,
             content: post.content,
-            eyecatch: post.eyecatch,
+            eyecatch: eyecatch,
             categories: post.categories,
             description: description,
+            prevPost: prevPost,
+            nextPost: nextPost,
         },
     }
 }
